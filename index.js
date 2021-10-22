532554573b9b5cb7164dd9e67a7e6cacc7fca8f8
Shipu8644 <shipu.monjurur@gmail.com>
2021-10-23 03:06:31 +0600

    cr done

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+const app = express();
 const port = process.env.PORT || 7000;
 
+//middleware
+app.use(cors());
+app.use(express.json());
+// user: mydbuser1
+// password: Avfyo1v7UkVrpm2s
 
-const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://mydbuser1:Avfyo1v7UkVrpm2s@cluster0.om5y9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
 
-    console.log('hitting the database');
-    const user = { name: "Shipu", gender: 'male' };
-    collection.insertOne(user)
-        .then(() => {
-            console.log('insert success')
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("ProductStore");
+        const productsCollection = database.collection("products");
+
+        // get Api
+        app.get('/products', async (req, res) => {
+            const cursor = productsCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
         })
-    //   client.close();
-});
+
+        // Post Api
+        app.post('/products', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productsCollection.insertOne(newProduct);
+            console.log('got the product', req.body);
+            console.log('added product', result);
+            res.json(result);
+        })
+
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
